c633d27c82b342cfaf5be9945e0649575c1dc7ab
Robert Holden <rob.holden@live.co.uk>
2021-12-31 16:47:57 +0000

    Added base path for identity

--- Backend/Microservices/Identity/Identity.Api/Startup.cs
+++ Backend/Microservices/Identity/Identity.Api/Startup.cs
@@ -23,6 +23,7 @@ public class Startup
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
+        app.UsePathBase("/identity");
         app.ConfigureStartup(Configuration, env, true);
     }
 
